e80988193d9c37de78ed1139de4cda5455340c31
emil237 <marianatalaat1@gmail.com>
2022-07-05 07:29:11 -0700

    Add files via upload

--- MetrixFHD.sh
+++ MetrixFHD.sh
@@ -1,106 +1,83 @@
 #!/bin/sh
 
 # wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Skin-Metrix-FHD/main/MetrixFHD.sh -O - | /bin/sh
-###################################################################################
-version=5
-################################################################################### 
+
+version=6
+#############################################################
 MY_URL="https://raw.githubusercontent.com/emil237/Skin-Metrix-FHD/main"
-METRIX1="Metrix1FHD.tar.gz"
-METRIX2="Metrix2FHD.tar.gz"
-MYTAR_PY3="MetrixFHD-py3.10.tar.gz"
-echo "******************************************************************************************************************"
+MYTAR_PY2="MetrixFHD-py2.0.tar.gz"
+MYTAR_PY3="MetrixFHD-py3.0.tar.gz"
+MYTAR_PY3.10="MetrixFHD-py3.10.tar.gz"
 TEMPATH=/tmp
 BOXHOSTNAME=$( cat /etc/hostname)
 SETTINGS='/etc/enigma2/settings'
+PYTHON_VERSION=$(python -c"import platform; print(platform.python_version())")
+
 
 # remove old version
 
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/MyMetrixLite
+rm -rf /usr/share/enigma2/MetrixHD
+
+
 echo ""
 # Download and install plugin
-cd /tmp
-set -e
-echo "===> Downloading And Installing MetrixFHD plugin Please Wait ......"
-if python --version 2>&1 | grep -q '^Python 3\.'; then
-  wget "$MY_URL/$MYTAR_PY3"
-tar -xzf MetrixFHD-py3.10.tar.gz -C /
-	else 
-echo "   Install Plugin please wait "
-   wget "$MY_URL/$METRIX1"
-wait
-tar -xzf Metrix1FHD.tar.gz -C /
-wait
-wget "$MY_URL/$METRIX2"
-wait
-tar -xzf Metrix2FHD.tar.gz -C /
-	fi
-echo "================================="
-set +e
-cd ..
-wait
-echo "================================="
-set +e
-cd ..
-wait
-rm -rf /tmp/Metrix1FHD.tar.gz
-rm -rf /tmp/Metrix2FHD.tar.gz
-rm -rf /tmp/MetrixFHD-py3.10.tar.gz
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+if [ "$PYTHON_VERSION" == 3.9.7 ]; then
+    echo "OpenAtv PYTHON-$PYTHON_VERSION ..."
+    cd /tmp
+    set -e
+    echo "===> Downloading And Installing MetrixFHD python3 plugin Please Wait ......"
+    echo
+    wget "$MY_URL/$MYTAR_PY3"
+    tar -xzf MetrixFHD-py3.0.tar.gz --warning=no-timestamp -C /
+    set +e
+    rm -f MetrixFHD-py3.0.tar.gz
+elif [ "$PYTHON_VERSION" == 3.10.4 ]; then
+    echo "OpenAtv PYTHON-$PYTHON_VERSION ..."
+    cd /tmp
+    set -e
+    echo "===> Downloading And Installing MetrixFHD python3.10 plugin Please Wait ......"
+    echo
+    wget "$MY_URL/$MYTAR_PY3.10"
+    tar -xzf MetrixFHD-py3.10.tar.gz --warning=no-timestamp -C /
+    set +e
+    rm -f MetrixFHD-py3.10.tar.gz    
+else
+    echo "OpenAtv PYTHON-$PYTHON_VERSION ..."
+    cd /tmp
+    set -e
+    echo "===> Downloading And Installing MetrixFHD python2 plugin Please Wait ......"
+    echo
+    wget "$MY_URL/$MYTAR_PY2"
+    tar -xzf MetrixFHD-py2.0.tar.gz -C /
+    set +e
+    rm -f MetrixFHD-py2.0.tar.gz   
 fi
 
 #########################
 echo "Add Setting To ${SETTINGS} ..."
-init 4
-sleep 2   
+init 4 
 {
-    echo "config.plugins.MetrixWeather.currentWeatherDataValid=0"
-    echo "config.plugins.MetrixWeather.currentWeatherfeelslike=24"
-    echo "config.plugins.MetrixWeather.currentWeatherhumidity=40"
-    echo "config.plugins.MetrixWeather.currentWeatherobservationtime=16:30:00"
-    echo "config.plugins.MetrixWeather.currentWeatherTemp=24"
-    echo "config.plugins.MetrixWeather.currentWeatherwinddisplay=5 mph South"
-    echo "config.plugins.MetrixWeather.currentWeatherwindspeed=5 mph"
-    echo "config.plugins.MetrixWeather.forecastTodayTempMax=25"
-    echo "config.plugins.MetrixWeather.forecastTomorrowTempMax3=19"
     echo "config.plugins.MyMetrixLiteOther.EHDenabled=1"
-    echo "config.plugins.MyMetrixLiteOther.EHDfontoffset=0"
     echo "config.plugins.MyMetrixLiteOther.EHDtested="$BOXHOSTNAME"_|_01"
-    echo "config.plugins.MyMetrixLiteOther.layeraunderlineposy=0"
-    echo "config.plugins.MyMetrixLiteOther.layeraunderlinesize=1"
-    echo "config.plugins.MyMetrixLiteOther.runningTextSpeed=60"
-    echo "config.plugins.MyMetrixLiteOther.runningTextStartdelay=1200"
-    echo "config.plugins.MyMetrixLiteOther.setFieldMargin=5"
-    echo "config.plugins.MyMetrixLiteOther.setItemDistance=5"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignButtonsFrameSize=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignButtonsTextPosition=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignButtonsTextSize=24"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignInfobarXPiconPosX=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignInfobarXPiconPosY=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignInfobarZZZPiconPosX=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignInfobarZZZPiconPosY=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignInfobarZZZPiconSize=0"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignScrollbarBorderWidth=1"
-    echo "config.plugins.MyMetrixLiteOther.SkinDesignScrollbarSliderWidth=10"
-    echo "config.plugins.MyMetrixLiteOther.STBDistance=10"        
+    
 } >>${SETTINGS}
 
 echo ""
 sync
 echo "##############################################################"
 echo "#         MetrixFHD $version INSTALLED SUCCESSFULLY          #"
-echo "#             Uploded BY Emil_Nabil           #"
+echo "#             Uploded BY EMIL_NABIL #"
+sleep 4;
 echo "##############################################################"
 echo "**************************************************************"
 echo "##############################################################"
 echo "#              your Device will RESTART Now                  #"
 echo "##############################################################"
-wait
-sleep 2
-init 4
-init 3
+if [ $OSTYPE = "DreamOS" ]; then
+    sleep 2
+    systemctl restart enigma2
+else
+    init 3
+fi
 exit 0
-
-
-
-
-
